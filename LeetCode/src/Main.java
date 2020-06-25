2835f4f3c4e0c68183850f0fc5d8699c9759132e
ZJPang <516585610@qq.com>
2020-06-25 09:30:56 +0800

    version bump to 0625

--- LeetCode/src/Main.java
+++ LeetCode/src/Main.java
@@ -1,9 +1,10 @@
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Main main = new Main();
-        System.out.println(main.threeSumClosest(new int[]{-1, 2, 1, -4}, 1));
+        System.out.println(main.wordBreak("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat")));
     }
 
     /**
@@ -83,5 +84,35 @@ public class Main {
             }
         }
         return res;
+    }
+
+    /**
+     * LC 139.单词拆分
+     * 传统DP问题
+     * 2020年6月25日
+     * @param s
+     * @param wordDict
+     * @return
+     */
+    public boolean wordBreak(String s, List<String> wordDict) {
+        //注意，这里必须要长一位，因为0号位会被作为标志位来使用。
+        boolean[] dp = new boolean[s.length()+1];
+        //dp[0]其实就是空字符串，空字符串必定在另外一个非空字符串的集合中。
+        dp[0] = true;
+        //外层的i用于控制当前字符串的右边界，所以会是小于等于s.length()
+        for(int i = 1; i <= s.length(); i++){
+            //内层的j用于控制左边界，慢慢去收缩这个边界。
+            for(int j = 0; j < i; j++){
+                //注意！这里判断条件有两个，第一个是当前剪切下来的字符串在字典中是否存在
+                //第二个是要检查我们dp[j]就是我们的前一位字符是否被正确匹配，如果这个不成立
+                //本次的收缩边界就有一个地方留下了间隙，不予执行。
+                if(dp[j] && wordDict.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        //返回最后那个位置的值
+        return dp[s.length()];
     }
 }
