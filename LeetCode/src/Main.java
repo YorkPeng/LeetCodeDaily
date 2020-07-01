fdc1991dff0edf8d2cd928d17e60b61889350064
ZJPang <516585610@qq.com>
2020-07-01 10:17:48 +0800

    version: bump to 0701

--- LeetCode/src/Main.java
+++ LeetCode/src/Main.java
@@ -247,5 +247,31 @@ public class Main {
         return right;
     }
 
+    /**
+     * LC 718.最长重复子数组
+     * 这是个经典DP问题。
+     * 主要是子数组和子序列的区别。
+     * 子序列默认不连续，子数组默认连续。
+     * 我们在状态转移的时候就有区别了，如果是子序列，如果A[i-1] != B[j-1]，我们dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1])
+     * 但是在子数组中，这个就不能这么写了，我们如果遇到不同的字符，直接跳过不判断即可。
+     * 同时也因为这个情况，我们最大值可能在整个二维DP数组中的某一个位置出现，所以需要一个res记录其最大值。
+     * 2020年7月1日
+     * @param A
+     * @param B
+     * @return
+     */
+    public int findLength(int[] A, int[] B) {
+        int[][] dp = new int[A.length+1][B.length+1];
+        int res = 0;
+        for(int i = 1; i < dp.length; i++){
+            for(int j = 1; j < dp[i].length; j++){
+                if(A[i-1] == B[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    res = Math.max(res,dp[i][j]);
+                }
+            }
+        }
+        return res;
+    }
 
 }
